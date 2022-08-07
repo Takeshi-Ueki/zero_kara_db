e71e783a13036ecfc3dadfe2d1ee987ac5bd081a
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2022-08-07 11:29:16 +0900

    4-1 Finish

---
+++ 4-1/list4-7.sql
@@ -0,0 +1,3 @@
+-- 暗黙的なデフォルト値の入力 列リストからhanbai_tankaを省略 VALUESからも省略
+INSERT INTO ShohinIns (shohin_id, shohin_mei, shohin_bunrui, shiire_tanka, torokubi)
+VALUES      ('0002', '穴あけパンチ', '事務用品', 320, '2009-09-11');
