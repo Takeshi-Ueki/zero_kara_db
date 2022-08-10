0b449ac66049e444f2f46eccc759562a7d43cc84
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2022-08-10 16:15:03 +0900

    5-2 Finish

---
+++ 5-2/list5-14.sql
@@ -0,0 +1,5 @@
+SELECT shohin_bunrui, AVG(hanbai_tanka)
+  FROM Shohin
+ GROUP BY shohin_bunrui
+HAVING AVG(hanbai_tanka) > (SELECT AVG(hanbai_tanka)
+                              FROM Shohin);
---
+++ 5-2/list5-9.sql
@@ -0,0 +1,6 @@
+-- サブクエリ
+
+SELECT shohin_bunrui, cnt_shohin
+  FROM (SELECT shohin_bunrui, COUNT(*) AS cnt_shohin
+          FROM Shohin
+         GROUP BY shohin_bunrui) AS ShohinSum;
