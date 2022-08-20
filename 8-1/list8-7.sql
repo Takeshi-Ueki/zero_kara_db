ee4b6f3bcd34647cdc4b2e1e5bff5a9b01c75e1a
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2022-08-20 09:51:33 +0900

    add

--- 8-1/list8-7.sql
+++ 8-1/list8-7.sql
@@ -1,5 +1,6 @@
--- カレントコードの２行前までの平均を表示(移動平均)
+-- カレントコードの前後１行との平均を表示(移動平均)
 SELECT shohin_id, shohin_mei, hanbai_tanka,
      AVG (hanbai_tanka) OVER (ORDER BY shohin_id
-                               ROWS 2 PRECEDING) AS moving_avg
+                               ROWS BETWEEN 1 PRECEDING AND
+                               1 FOLLOWING) AS moving_avg
   FROM Shohin;
--- 8-1/list8-8.sql
+++ 8-1/list8-8.sql
@@ -1,3 +1,4 @@
 SELECT shohin_mei, shohin_bunrui, hanbai_tanka,
        RANK () OVER (ORDER BY hanbai_tanka) AS ranking
-  FROM Shohin;
+  FROM Shohin
+ ORDER BY ranking;
